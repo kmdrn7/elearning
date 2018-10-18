5a9aeaf34a70ad5420578973642e9f768f11f81e
Andika Ahmad Ramadhan <ASpendaka@gmail.com>
2018-10-19 05:20:59 +0700

    add remark material admin template

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,26 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * 
+ * BELOW IS DEFAULT ROUTE :))
+ */
+
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+/**
+ * 
+ * BELOW IS FRONT-END ROUTE :))
+ * : rute buat tampilan depan
+ */
+
+
+/**
+ * 
+ * BELOW IS BACK-END ROUTE :))
+ * : rute untuk admin panel
+ */
+
+$route['admin'] = 'admin/AdminController/index';
